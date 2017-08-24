0cc88c9f77845b1c3d14975fcd0a53854fbc4c81
Cosimo Cecchi <cosimo@endlessm.com>
2017-08-23 18:56:49 -0700

    application: make sure to create a window in activate()
    Now that we're dbus activatable, make sure we create a window when
    activated.

--- src/yelp-application.c
+++ src/yelp-application.c
@@ -205,7 +205,8 @@ yelp_application_init (YelpApplication *app)
 static void
 yelp_application_activate (GApplication *application)
 {
-    /* Here we don't need to do anything else. */
+    YelpApplication *app = YELP_APPLICATION (application);
+    open_uri (app, yelp_uri_new (YELP_GNOME_HELP_URI), FALSE, TRUE);
 }
 
 static void
@@ -470,7 +471,7 @@ yelp_application_command_line (GApplication            *application,
     argv = g_application_command_line_get_arguments (cmdline, NULL);
 
     if (argv[1] == NULL)
-        open_uri (app, yelp_uri_new (YELP_GNOME_HELP_URI), FALSE, TRUE);
+        g_application_activate (application);
 
     for (i = 1; argv[i]; i++)
         open_uri (app, yelp_uri_new (argv[i]), FALSE, FALSE);
